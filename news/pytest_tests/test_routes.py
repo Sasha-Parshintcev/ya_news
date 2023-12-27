f15a98392acf2bfbbe9bf4ad85b0cdd0e20ab34b
Саша Паршинцев <aleksandr-par@mail.ru>
2023-12-27 12:11:47 +0700

    add pytest routes

--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -1,13 +1,117 @@
-# Главная страница доступна анонимному пользователю.
+from http.client import OK, NOT_FOUND
+import pytest
+from pytest_django.asserts import assertRedirects
 
-# Страница отдельной новости доступна анонимному пользователю.
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Страницы удаления и редактирования комментария доступны автору комментария.
+from news.models import News, Comment
 
-# При попытке перейти на страницу редактирования или удаления комментария анонимный
-# пользователь перенаправляется на страницу авторизации.
+User = get_user_model()
 
-# Авторизованный пользователь не может зайти на страницы редактирования или удаления
-# чужих комментариев (возвращается ошибка 404).
+@pytest.fixture
+def user(django_user_model):  
+    return django_user_model.objects.create(username='Пользователь')
 
-# Страницы регистрации пользователей, входа в учётную запись и выхода из неё доступны анонимным пользователям.
+@pytest.fixture
+def user_client(user, client):
+    client.force_login(user)
+    return client
+
+@pytest.fixture
+def author(django_user_model):  
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def author_client(author, client):
+    client.force_login(author)
+    return client
+
+@pytest.fixture
+def news():
+    news = News.objects.create(
+        title='Заголовок',
+        text='Текст заметки'
+    )
+    return news 
+
+@pytest.fixture
+def comment(news, author):
+    comment = Comment.objects.create(
+        news=news,
+        author=author,
+        text='Текст комментария'
+    )
+    return comment
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'name',
+    ('news:home', 'users:login', 'users:logout', 'users:signup')
+)
+def test_pages_availability_for_anonymous_user(client, name):
+    """
+    Главная страница, страницы регистрации пользователей, входа в учётную
+    запись и выхода из неё доступны анонимным пользователям.
+    """
+    url = reverse(name)
+    response = client.get(url)
+    assert response.status_code == OK
+
+@pytest.mark.django_db
+def test_pages_availability_for_author(client, news):
+    """
+    Страница отдельной новости доступна анонимному пользователю, а
+    авторизованный пользователь не может зайти на страницы редактирования
+    или удаления чужих комментариев.
+    """
+    url = reverse('news:detail', args=(news.pk,))
+    response = client.get(url)
+    assert response.status_code == OK 
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    (
+        (pytest.lazy_fixture('user_client'), NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), OK)
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    ('news:edit', 'news:delete'),
+)
+def test_pages_availability_for_author(
+    parametrized_client, name, comment, expected_status
+    ):
+    """
+    Страницы удаления и редактирования комментария доступны автору комментария.
+    """
+    url = reverse(name, args=(comment.pk,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status 
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'name, note_object',
+    (
+        ('news:edit', pytest.lazy_fixture('comment')),
+        ('news:delete', pytest.lazy_fixture('comment')),
+    ),
+)
+def test_redirects(client, name, note_object):
+    """
+    При попытке перейти на страницу редактирования или удаления комментария
+    анонимный пользователь перенаправляется на страницу авторизации.
+    """
+    login_url = reverse('users:login')
+    if note_object is not None:
+        url = reverse(name, args=(note_object.pk,))
+    else:
+        url = reverse(name)
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url)
